f3ff2cdde032155e6c73f9c2007b3e8c3f49a4f1
John Hamrick <john@hamrickjohn.me>
2014-12-16 14:05:58 -0500

    CRUD working code

--- app/views/category_add.blade.php
+++ app/views/category_add.blade.php
@@ -1,34 +1,14 @@
 @extends('_master')
-
 @section('title')
-		Add a new task
+Add a new book
 @stop
-
 @section('head')
-
 @stop
-
 @section('content')
-
-		<h1>Add a new task</h1>
-
-		{{ Form::open(array('url' => '/task/create')) }}
-			
-			{{ Form::label('task','Task') }}
-			{{ Form::text('task'); }}
-
-			{{ Form::label('user_id', 'User') }}
-			{{ Form::select('user_id', $users); }}
-
-			{{ Form::label('created','Created (Y-m-d)') }}
-			{{ Form::text('created'); }}
-
-			@foreach($tags as $id => $tag)
-				{{ Form::checkbox('tags[]', $id); }} {{ $tag }}
-			@endforeach
-
-			{{ Form::submit('Add'); }}
-
-	{{ Form::close() }}
-
+<h1>Add a new category name</h1>
+{{ Form::open(array('url' => '/category/create')) }}
+{{ Form::label('name','Category Name') }}
+{{ Form::text('name'); }}
+{{ Form::submit('Add'); }}
+{{ Form::close() }}
 @stop
--- app/views/category_edit.blade.php
+++ app/views/category_edit.blade.php
@@ -1,31 +1,19 @@
 @extends('_master')
-
 @section('title')
-     Edit Category
+Edit
 @stop
-
 @section('head')
-
 @stop
-
 @section('content')
-
-    <h1>Edit</h1>
-        
-        <h2>{{{ $category['name'] }}}</h2>
-
-    {{---- EDIT  ----}}
-
-    {{ Form::open(array('url' => '/category/edit')) }}
-
-    {{ Form::hidden('id' ,$category['id']); }}
-
-        
-      <div class='form-group'>
-              {{ Form::label('name', 'Name') }}
-              {{ Form::text('name' ,$category['name']); }}
-      </div>
-
-              {{ Form::submit('Update') }}
-    
-    {{ Form::close() }}
+<h1>Edit</h1>
+<h2>{{{ $category['name'] }}}</h2>
+{{---- EDIT -----}}
+{{ Form::open(array('url' => '/category/edit')) }}
+{{ Form::hidden('id',$category['id']); }}
+<div class='form-group'>
+{{ Form::label('name','Name') }}
+{{ Form::text('name',$category['name']); }}
+</div>
+{{ Form::submit('Save'); }}
+{{ Form::close() }}
+@stop
